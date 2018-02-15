89d6a76e1f0b1eb0537cb062a6520dc9b01b81d5
Imam Susanto <abc.imams@gmail.com>
2018-02-15 18:45:23 +0700

    Use Babel Minify and PurgeCSS

--- poi.config.js
+++ poi.config.js
@@ -1,10 +1,35 @@
 const OfflinePlugin = require('offline-plugin')
+const PurgeCSS = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
 
 module.exports = options => ({
   entry: './src/main.js',
+  presets: [
+    // require('poi-preset-buble')(),
+    require('poi-preset-babel-minify')()
+  ],
+  sourceMap: false,
   extendWebpack(config) {
     if (options.mode === 'production') {
       config.plugin('offline').use(OfflinePlugin)
+      config.plugin('purgecss').use(PurgeCSS, [{
+        paths: glob.sync([
+          path.join(__dirname, 'src/**/*.vue')
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ['html', 'js', 'vue']
+          }
+        ]
+      }])
     }
   }
 })
